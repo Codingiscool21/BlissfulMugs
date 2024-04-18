40e2111d60f93de2a2c0e2bfa6b79377d3b8f54e
NirVanm <87303697+Codingiscool21@users.noreply.github.com>
2024-04-18 21:03:52 +1000

    Update Server.py

--- Server.py
+++ Server.py
@@ -2,13 +2,13 @@ import streamlit as st
 import numpy as np
 
 st.title("Blissful Mugs")
-st.markdown("Upload an Imge and we will print it on your mug")
+st.markdown("Upload an Image and we will print it on your mug")
 
 img_file_buffer = st.file_uploader("Upload a PNG image", type='png')
 
 if img_file_buffer is not None:
   image = Image.open(img_file_buffer)
-  img_array = np.array(Image)
+  img_array = np.array(image)
 
 
 
