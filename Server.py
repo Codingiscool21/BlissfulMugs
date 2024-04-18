dd330f8f0336ab1c8cd1df031e3bb12e24e244e6
NirVanm <87303697+Codingiscool21@users.noreply.github.com>
2024-04-18 20:40:15 +1000

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.markdown("", unsafe-allow-html=true)
+page_bg_color = """
+<style>
+[data-testid="stAppViewContainer"]{
+background-color: #FFFFFF;
+}
+</style>
+"""
+
+st.markdown(page_bg_color, unsafe-allow-html=True)
+st.title("Blissful Mugs")
