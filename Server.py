1a8f34e56a5ccad558a93ed812a1efb1bb816001
NirVanm <87303697+Codingiscool21@users.noreply.github.com>
2024-04-19 08:46:20 +1000

    Update Server.py

--- Server.py
+++ Server.py
@@ -3,10 +3,12 @@ import numpy as np
 from PIL import Image
 
 st.title("Blissful Mugs")
-st.markdown("This image will be printed on your mug")
+
 
 img_file_buffer = st.file_uploader("Upload a PNG image", type='png')
 
 if img_file_buffer is not None:
   image = Image.open(img_file_buffer)
   img_array = np.array(image)
+
+st.markdown("This image will be printed on your mug")
